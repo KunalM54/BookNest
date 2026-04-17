45c19edf89312e1b91512a6544a392b4bfa36615
KunalM54 <kunalmendapara@gmail.com>
2026-04-17 16:27:59 +0530

    Fix forgot password not working feature

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
-import 'zone.js';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+
+
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
